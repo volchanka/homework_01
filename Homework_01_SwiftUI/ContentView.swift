999868035d53c0815a7c6a514d8fdf730a2a8707
Lyudmila Danilchenko <ordoko@gmail.com>
2020-06-03 16:23:40 +0300

    Initial Commit

---
+++ Homework_01_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework_01_SwiftUI
+//
+//  Created by Даня on 03.06.2020.
+//  Copyright © 2020 Be Wind Lab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
